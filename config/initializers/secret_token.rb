62ac9074c8b610342ad21fa5f0a7334bcde5865a
David Patnode <draknor@gmail.com>
2012-11-21 09:24:19 +0000

    Finished first tutorial; did not implement features for all cucumber tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsappsTutorial::Application.config.secret_token = '78e82268d782cfcc59934fc822c31933d2e7d6cb29a4bdad4686ffc600f00e0e83a435c0c003375fe933ded406d7c84f31e1a95a4614146a4408c5ea694acc66'
